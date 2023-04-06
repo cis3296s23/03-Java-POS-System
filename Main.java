82c87f9cdb4b3b4f99f5c4de95e6a92881b69aca
Ziyike1 <tul51428@temple.edu>
2023-04-06 18:59:33 -0400

    fix some error about main program

--- Main.java
+++ Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println("Please enter your password: ");
         String restPassWord = scanner.nextLine();
 
-        if (restUserName.equals(rest1Name) && restPassWord.equals(pass1)){
+        if (restUserName.equals(rest1Name) && restPassWord.equals(pass1)) {
             System.out.println("\n******LOGIN SUCCESSFUL*****\n");
             System.out.println("WELCOME TO DAVE'S BURGER");
             System.out.println("Enter Username: ");
@@ -23,14 +23,14 @@ public class Main {
             System.out.println("Enter Password: ");
             String pass = scanner.nextLine();
 
-            if(user.equals("admin")&& pass.equals("admin123")){
+            if (user.equals("admin") && pass.equals("admin123")) {
                 System.out.println("1. View Sale Report");
                 System.out.println("2. Take Order");
                 System.out.println("3. View Timecards");
                 System.out.println("4. Inventory");
                 System.out.println("Please enter your choice");
                 int choice = scanner.nextInt();
-                switch(choice){
+                switch (choice) {
                     case 1:
                         break;
                     case 2:
@@ -45,8 +45,7 @@ public class Main {
                         System.out.println("Invalid choice!");
                         break;
                 }
-            }
-            else {
+            } else {
                 System.out.println("1. Clock-in to start working");
                 System.out.println("2. Create Order");
                 System.out.println("3. View Menu");
@@ -78,7 +77,7 @@ public class Main {
                             empClockIns.put(empInfo, currentTime);
                             System.out.println("You have clocked in at " + currentTime + ".");
                         }
-                        break;//employee can clock-in
+                        break;
                     case 2:
                         break;
                     case 3:
@@ -89,59 +88,10 @@ public class Main {
                         System.out.println("Invalid choice!");
                         break;
                 }
-=======
-                    case 1:break;//do sth;
-                    case 2:break;
-                    case 3:break;
-                    case 4:break;
-                    case 0:break;
-                }
             }
-            else {
-                    System.out.println("1. Clock-in to start working");
-                    System.out.println("2. Create Order");
-                    System.out.println("3. View Menu");
-                    System.out.println("Enter your selection: ");
-                    int choice = scanner.nextInt();
-                    switch (choice) {
-                        case 1:
-                            HashMap<String, LocalTime> empClockIns = new HashMap<>();
-                            HashMap<String, LocalTime> employeeClockOuts = new HashMap<>();
-                            System.out.println("Please enter your 4 digits employee number: ");
-                            String empInfo = scanner.nextLine();
-
-                            if (empClockIns.containsKey(empInfo)) {
-                                System.out.println("You've already clock in today at " + empClockIns.get(empInfo) + ".");
-                                System.out.println("Enter 'out' to clock out: ");
-                                String cOut = scanner.nextLine();
-                                if (cOut.equalsIgnoreCase("out")) {
-                                    LocalTime currentTime = LocalTime.now();
-                                    employeeClockOuts.put(empInfo, currentTime);
-                                    Duration totalHours = Duration.between(empClockIns.get(empInfo)
-                                            , employeeClockOuts.get(empInfo));
-                                    long hours = totalHours.toHours();
-                                    long minutes = totalHours.toMinutes();
-                                    System.out.println("You have worked " + hours + " hours and " + minutes + " minutes today.");
-                                }
-                            } else {
-                                // Record employee clock-in time
-                                LocalTime currentTime = LocalTime.now();
-                                empClockIns.put(empInfo, currentTime);
-                                System.out.println("You have clocked in at " + currentTime + ".");
-                            }
-                            break;//employee can clock-in
-                        case 2:
-                            break;
-                        case 3:
-                            break;
-                        case 0:
-                            break;
-                    }
-            }
-
-        }
-        else{
+        } else {
             System.out.println("\n******LOGIN FAILED******\n");
-     }
+        }
     }
 }
+
