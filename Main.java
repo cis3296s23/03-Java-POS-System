de9bb7e73127be42fb9ca6fccb954968639e2dc6
Ziyike1 <tul51428@temple.edu>
2023-04-06 22:07:14 -0400

    add the log-out button for user exit the page of choosing

--- Main.java
+++ Main.java
@@ -9,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String rest1Name = "daveburger";
-        String pass1 = "dave123";
+        //change the username and password for easier test
+        String rest1Name = "a";
+        String pass1 = "1";
 
         System.out.println("Please enter your username: ");
         String restUserName = scanner.nextLine();
@@ -82,72 +83,81 @@ public class Main {
                         break;
                 }
             } else {
-                System.out.println("1. Clock-in to start working");
-                System.out.println("2. Create Order");
-                System.out.println("3. View Menu");
-                System.out.println("Enter your selection: ");
-                int choice = scanner.nextInt();
-                scanner.nextLine();
-                switch (choice) {
-                    case 1:
-                        System.out.println("1. Clock-in to start working");
-                        System.out.println("2. Clock-out to finish working");
-                        System.out.println("Please enter your 4 digits employee number: ");
-                        String empInfo = scanner.nextLine();
-                        int choice2 = scanner.nextInt();
-                        if (choice2 == 1) {
-                            empClockIns.put(empInfo, LocalTime.now());
-                            System.out.println("You have successfully clocked-in.");
-                        } else if (choice2 == 2) {
-                            empClockOuts.put(empInfo, LocalTime.now());
-                            System.out.println("You have successfully clocked-out.");
-                        }
-                        if (empClockIns.containsKey(empInfo)) {
-                            LocalTime clockInTime = empClockIns.get(empInfo);
-                            LocalTime clockOutTime = empClockOuts.get(empInfo);
-                            if (clockOutTime == null) {
-                                Duration totalHours = Duration.between(clockInTime, LocalTime.now());
-                                long hours = totalHours.toHours();
-                                long minutes = totalHours.toMinutes() % 60;
-                                System.out.println("You have worked " + hours + " hours and " + minutes + " minutes today.");
-                                System.out.println("You clocked in at " + clockInTime + ".");
-                            } else {
-                                Duration totalHours = Duration.between(clockInTime, clockOutTime);
-                                long hours = totalHours.toHours();
-                                long minutes = totalHours.toMinutes() % 60;
-                                System.out.println("You worked " + hours + " hours and " + minutes + " minutes on your last shift.");
-                                System.out.println("You clocked in at " + clockInTime + " and clocked out at " + clockOutTime + ".");
+                boolean continueWorking = true;
+                while (continueWorking) {
+                    System.out.println("1. Clock-in to start working");
+                    System.out.println("2. Create Order");
+                    System.out.println("3. View Menu");
+                    System.out.println("4. Log Out");
+                    System.out.println("Enter your selection: ");
+                    int choice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    switch (choice) {
+                        case 1:
+                            System.out.println("Please enter your 4 digits employee number: ");
+                            String empInfo = scanner.nextLine();
+                            System.out.println("1. Clock-in to start working");
+                            System.out.println("2. Clock-out to finish working");
+                            System.out.println("Enter your selection: ");
+                            int choice2 = scanner.nextInt();
+                            if (choice2 == 1) {
+                                empClockIns.put(empInfo, LocalTime.now());
+                                System.out.println("You have successfully clocked-in.");
+                            } else if (choice2 == 2) {
+                                empClockOuts.put(empInfo, LocalTime.now());
+                                System.out.println("You have successfully clocked-out.");
                             }
-                        } else {
-                            System.out.println("You have not clocked in today.");
-                        }
-                        break;
-                    case 2:
-                        System.out.println("Enter customer name: ");
-                        String customerName = scanner.nextLine();
-                        System.out.println("Enter order details: ");
-                        String orderDetails = scanner.nextLine();
-                        System.out.println("Enter total price: ");
-                        double totalPrice = scanner.nextDouble();
-                        scanner.nextLine();
-                        System.out.println("Enter payment method: ");
-                        String paymentMethod = scanner.nextLine();
-                        System.out.println("Order created successfully!");
-                        break;
-                    case 3:
-                        System.out.println("***********MENU***********");
-                        System.out.println("1. Beef Burger - $11.99");
-                        System.out.println("2. Cheeseburger - $9.99");
-                        System.out.println("3. Hamburger - $8.99");
-                        System.out.println("4. French Fries - $4.99");
-                        System.out.println("5. Soft Drink - $1.99");
-                        System.out.println("6. Milkshake - $5.99");
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        System.out.println("Invalid choice!");
-                        break;
+                            if (empClockIns.containsKey(empInfo)) {
+                                LocalTime clockInTime = empClockIns.get(empInfo);
+                                LocalTime clockOutTime = empClockOuts.get(empInfo);
+                                if (clockOutTime == null) {
+                                    Duration totalHours = Duration.between(clockInTime, LocalTime.now());
+                                    long hours = totalHours.toHours();
+                                    long minutes = totalHours.toMinutes() % 60;
+                                    System.out.println("You have worked " + hours + " hours and " + minutes + " minutes today.");
+                                    System.out.println("You clocked in at " + clockInTime + ".");
+                                } else {
+                                    Duration totalHours = Duration.between(clockInTime, clockOutTime);
+                                    long hours = totalHours.toHours();
+                                    long minutes = totalHours.toMinutes() % 60;
+                                    System.out.println("You worked " + hours + " hours and " + minutes + " minutes on your last shift.");
+                                    System.out.println("You clocked in at " + clockInTime + " and clocked out at " + clockOutTime + ".");
+                                }
+                            } else {
+                                System.out.println("You have not clocked in today.");
+                            }
+                            break;
+                        case 2:
+                            System.out.println("Enter customer name: ");
+                            String customerName = scanner.nextLine();
+                            System.out.println("Enter order details: ");
+                            String orderDetails = scanner.nextLine();
+                            System.out.println("Enter total price: ");
+                            double totalPrice = scanner.nextDouble();
+                            scanner.nextLine();
+                            System.out.println("Enter payment method: ");
+                            String paymentMethod = scanner.nextLine();
+                            System.out.println("Order created successfully!");
+                            break;
+                        case 3:
+                            System.out.println("***********MENU***********");
+                            System.out.println("1. Beef Burger - $11.99");
+                            System.out.println("2. Cheeseburger - $9.99");
+                            System.out.println("3. Hamburger - $8.99");
+                            System.out.println("4. French Fries - $4.99");
+                            System.out.println("5. Soft Drink - $1.99");
+                            System.out.println("6. Milkshake - $5.99");
+                            break;
+                        case 4:
+                            continueWorking = false;
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Invalid choice!");
+                            break;
+                    }
                 }
             }
         } else {
