8f893cee95374569ade6843f07b452086de930c1
Cynthia To <tuh30326@temple.edu>
2023-04-14 11:40:55 -0400

    created the design for the payment method page using the scenebuilder

--- src/AddPayment.java
+++ src/AddPayment.java
@@ -1,20 +1,25 @@
 package src;
 
+import java.net.URL;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.ResourceBundle;
 import java.util.Scanner;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
@@ -25,20 +30,18 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 //extends application for JavaFX
 
-public class AddPayment extends Application{
+public class AddPayment implements Initializable {
+    @FXML
+    private Label davesBurgerLabel, paymentMethodLabel;
+
+    @FXML
+    private RadioButton cashRadioBtn, cardRadioBtn;
+
+    @FXML
+    private Button nextButton;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("paymentMethod.fxml"));
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+    public void initialize(URL location, ResourceBundle resources) {
 
-    public static void main(String[] args){
-        launch(args);
-    }
-
-    private AnchorPane createPaymentMethodPane(){
-
-        return null;
     }
 }
