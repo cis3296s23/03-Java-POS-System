8b40d41bdb025192fc80b26acaa331d8514bb1a6
Leapheng Keam <tuk74678@temple.edu>
2023-04-22 22:40:58 -0400

    Redesigned the login page and connected multiple scene to successfully, refractored some codes

--- src/DashboardController.java
+++ src/DashboardController.java
@@ -1,23 +1,65 @@
 package src;
 
-
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
-public class DashboardController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private AnchorPane scenePane;
+public class DashboardController implements Initializable {
 
     @FXML
-    private Button menu, updateMenu, balance, clockIn, report, transaction, setting, logout;
+    private Button adminAccess_btn;
 
     @FXML
-    Stage stage;
+    private TableColumn<?, ?> cardEnd_col;
 
+    @FXML
+    private Button clockInOut_btn;
+
+    @FXML
+    private Button clockIn_btn;
+
+    @FXML
+    private Button clockOut_btn;
+
+    @FXML
+    private TableColumn<?, ?> cusName_col;
+
+    @FXML
+    private Button menu_btn;
+
+    @FXML
+    private TableColumn<?, ?> orderId_col;
+
+    @FXML
+    private TableColumn<?, ?> orderNum_col;
+
+    @FXML
+    private TableColumn<?, ?> payment_col;
+
+    @FXML
+    private TableColumn<?, ?> timeOrdered_col;
+
+    @FXML
+    private TableColumn<?, ?> total_col;
+
+    @FXML
+    private Button transaction_btn;
+
+    @FXML
+    private AnchorPane transaction_form;
+    public void adminAccess_btn() throws IOException {
+        Main main = new Main();
+        main.changeScene("LoginPage.fxml", "Dave's Burger");
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
 }
