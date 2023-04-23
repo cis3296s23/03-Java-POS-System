34c71965fa32d7cd30cdc117f9ac5fd62e357141
Ziyike1 <tul51428@temple.edu>
2023-04-23 18:42:39 -0400

    create the chefs fxml page with its code

--- src/Chefs.java
+++ src/Chefs.java
@@ -1,26 +1,37 @@
 package src;
 
+import java.io.IOException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
 public class Chefs {
-    private int chefID;
-    private String chefName;
 
+    private Stage stage;
 
-    public Chefs(){
-
+    public Chefs(Stage stage) {
+        this.stage = stage;
     }
 
-    public Chefs(int chefID, String chefName){
-        this.chefID = chefID;
-        this.chefName = chefName;
+    public void showChefsPage(Order order) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("ChefsPage.fxml"));
+        BorderPane root = loader.load();
+
+        // Get the table view from the FXML file
+        TableView<Item> orderTable = (TableView<Item>) root.lookup("#orderTable");
+
+        // Populate the table view with the items from the order
+        ObservableList<Item> items = FXCollections.observableArrayList();
+        orderTable.setItems(items);
+
+        // Set the new scene in the main stage
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Chefs Page");
+        stage.show();
     }
-
-
-  public void receiveOrder(){
-
-  }
-
-  public void restockItems(){
-
-  }
-
 }
