90f6fc421c92b761329fc4ce905243a3d3dfecea
Leapheng Keam <tuk74678@temple.edu>
2023-04-25 00:52:43 -0400

    Completed the functionality on Dashboard by showing the current date transaction with its details. Refactored some junk codes and the program runs as expected. Redesigned the transaction page for better visual.

--- src/Main.java
+++ src/Main.java
@@ -12,90 +12,9 @@ import javafx.stage.Stage;
 public class Main extends Application {
     private static Stage stage;
     public static void main(String[] args) {
-//        //an array of string to hold the menu data
-//        String[][] menuItems = {
-//                {"1", "Bacon Burger", "Burgers", "13.00"},
-//                {"2", "Salmon BLT", "Burgers", "16.75"},
-//                {"3", "Brunch Burger", "Burgers", "16.80"},
-//                {"4", "The Goat", "Burgers", "17.00"},
-//                {"5", "Chicken Cheesesteak", "Steak", "13.00"},
-//                {"6", "Phillys Cheesesteak", "Steak", "15.00"},
-//                {"7", "Salmon Rolls", "EggRolls", "10.00"},
-//                {"8", "Shrimp Rolls", "EggRolls", "10.00"},
-//                {"9", "Chicken Rolls", "Egg Rolls", "8.00"},
-//                {"10", "Extra Mayo", "Extras", "1.00"},
-//                {"11", "Sauteed Spinach", "Extras", "1.00"},
-//                {"12", "Turkey Bacon", "Extras", "2.00"},
-//                {"13", "Beef Bacon", "Extras", "2.00"},
-//                {"14", "Extra Cheese", "Extras", "1.50"},
-//                {"15", "Teriyaki Sauce", "Side Sauce", "1.00"},
-//                {"16", "Sweet Chilli Sauce", "Side Sauce", "1.00"},
-//                {"17", "Extra BBQ Sauce", "Side Sauce", "1.00"},
-//                {"18", "Sweet Potato Dipping Sauce", "Side Sauce", "1.00"},
-//                {"19", "Wraps", "Wraps and Salads", "10.00"},
-//                {"20", "Salads", "Wraps and Salads", "10.00"},
-//                {"21", "Lemonade", "Drinks", "3.00"},
-//                {"22", "Soft Drinks", "Drinks", "3.00"},
-//                {"23", "Water", "Drink", "2.00"},
-//                {"24", "Bacon Burger", "Burgers", "13.00"},
-//                {"25", "Salmon BLT", "Burgers", "16.75"},
-//                {"26", "Brunch Burger", "Burgers", "16.80"},
-//                {"27", "The Goat", "Burgers", "17.00"},
-//                {"28", "Salmon Rolls", "EggRolls", "10.00"},
-//                {"29", "Shrimp Rolls", "EggRolls", "10.00"},
-//                {"30", "Chicken Rolls", "Egg Rolls", "8.00"},
-//                {"31", "Extra Mayo", "Extras", "1.00"},
-//                {"32", "Sauteed Spinach", "Extras", "1.00"},
-//                {"33", "Turkey Bacon", "Extras", "2.00"},
-//                {"34", "Beef Bacon", "Extras", "2.00"},
-//                {"35", "Extra Cheese", "Extras", "1.50"},
-//                {"36", "Teriyaki Sauce", "Side Sauce", "1.00"},
-//                {"37", "Sweet Chilli Sauce", "Side Sauce", "1.00"},
-//                {"38", "Extra BBQ Sauce", "Side Sauce", "1.00"},
-//                {"39", "Sweet Potato Dipping Sauce", "Side Sauce", "1.00"},
-//                {"40", "Wraps", "Wraps and Salads", "10.00"},
-//                {"41", "Salads", "Wraps and Salads", "10.00"},
-//                {"42", "Lemonade", "Drinks", "3.00"},
-//                {"43", "Soft Drinks", "Drinks", "3.00"},
-//                {"44", "Water", "Drink", "2.00"}};
-//
-//        //an array of string to hold employees data
-//        String[][] employees = {{"1111", "John Smith", "john@temple.edu", "16.00", "Cashier"},
-//                {"1112", "Jane Doe", "jane@temple.edu", "16.00", "Cashier"},
-//                {"1113", "Kyle Walker", "kyle@temple.edu", "20.00", "Chef"},
-//                {"1114", "Jack Grealish", "jack@temple.edu", "25.00", "HeadChef"}};
-//
-//        String[][] orders = {{"1", ""}};
-//        String[][] items = {{}};
 
         System.out.println("\tDATABASE CREATION");
 
-/*        String driver = "com.mysql.cj.jdbc.Driver";
-        String url = "jdbc:mysql://localhost";
-        //make sure your local server is on and username and password are the according to this below
-        String user = "root";
-        String password = "root1234";*/
-
-/*
-        //create an instance for Database class
-        Database database = null;
-
-        try {
-            database = new Database(url, user, password, menuItems, employees);
-            database.createDatabase();
-            System.out.println("***************************************");
-        } catch (SQLException e) {
-            System.err.println("Database error: " + e.getMessage());
-        } finally {
-            if (database != null) {
-                try {
-                    database.close();
-                } catch (SQLException e) {
-                    System.err.println("Error closing database connection: " + e.getMessage());
-                }
-            }
-        }*/
-        //for start()
         launch(args);
     }
     @Override
--- src/Order.java
+++ src/Order.java
@@ -1,87 +1,38 @@
 package src;
 
+import java.sql.Date;
+import java.sql.Time;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Order {
-    private String customerName;
-    private List<String> items = new ArrayList<>();
-    private LocalDateTime orderTime;
-    private double totalPrice;
-    private String paymentMethod;
+    private int order_id;
+    private String order_name;
+    private double order_total;
+    private String payment_type;
+    private String card_ending;
+    private Time time_ordered;
 
-    public Order(String customerName, List<String> items, double totalPrice, String paymentMethod) {
-        this.customerName = customerName;
-        this.items = items;
-        this.totalPrice = totalPrice;
-        this.paymentMethod = paymentMethod;
-        this.orderTime = LocalDateTime.now();
-    }
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
+    public Order(int order_id, String order_name,
+                 double order_total, String payment_type, String card_ending, Time time_ordered) {
+        this.order_id = order_id;
+        this.order_name = order_name;
+        this.order_total = order_total;
+        this.payment_type = payment_type;
+        this.card_ending = card_ending;
+        this.time_ordered = time_ordered;
     }
 
-    public List<String> getItems() {
-        return items;
+    public Order(int orderId, String customerName, double orderTotal, String position) {
     }
 
-    public void setItems(List<String> items) {
-        this.items = items;
+    public int getOrder_id() {
+        return order_id;
     }
-
-
-    public LocalDateTime getOrderTime() {
-        return orderTime;
-    }
-
-    public void setOrderTime(LocalDateTime orderTime) {
-        this.orderTime = orderTime;
-    }
-
-    public double getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-
-
-    public void addItem(String item) {
-        this.items.add(item);
-    }
-
-    public void removeItem(String item) {
-        this.items.remove(item);
-    }
-
-    public void clearItems() {
-        this.items.clear();
-    }
-
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "customerName='" + customerName + '\'' +
-                ", items=" + items +
-                ", orderTime=" + orderTime +
-                ", totalPrice=" + totalPrice +
-                ", paymentMethod='" + paymentMethod + '\'' +
-                '}';
-    }
+    public String getOrder_name(){return order_name;}
+    public double getOrder_total(){return order_total;}
+    public String getPayment_type(){return payment_type;}
+    public String card_ending(){return card_ending;}
+    public Time getTime_ordered(){return time_ordered;}
 }
